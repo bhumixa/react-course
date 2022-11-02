da1cda253bf9808f3db00f0d67690d0ff5c198da
bhumixa <bhumi.patel2512@gmail.com>
2022-11-02 21:25:57 +0530

    react routes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,50 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from 'react';
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   const [mode, changeMode] = useState("light")
 
   const toggleMode = () => {
     if (mode === 'light') {
       changeMode("dark")
+      document.body.style.backgroundColor = 'grey'
     } else {
       changeMode("light")
+      document.body.style.backgroundColor = 'white'
     }
   }
+
+
   return (
     <>
-      <Navbar title="h1" mode={mode} toggleMode={toggleMode}/>
+
+
+
+      <BrowserRouter>
+        <Navbar title="h1" mode={mode} toggleMode={toggleMode} />
+        <Routes>
+          <Route path="/" element={<TextForm heading="Enter Text" mode={mode} />}>
+            
+          </Route>
+          <Route path="About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Routes>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <TextForm heading="Enter Text" mode={mode} />
+        </Route>
+      </Routes> */}
+
+
       {/* <About /> */}
-      <TextForm heading="Enter Text" mode={mode}  />
+
       {/* <div className="container">
         <button onClick={changeMode} className="btn btn-primary" type="button"  >
           Toggle Theme
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,30 +1,47 @@
 import React, { useState } from 'react'
 
-
-
 export default function TextForm(props) {
     const [text, setText] = useState("");
     const [uText, setUtext] = useState("");
     const [reverseText, setReverse] = useState("");
-    
+    const [textAreastyling, settextAreastyling] = useState({
+        color: 'black',
+        backgroundColor: 'grey'
+    });
+
+    // if(props.mode === 'light'){
+    //     debugger
+    //     let obj = {
+    //         color:'black',
+    //         backgroundColor:'grey'
+    //     }
+    //     settextAreastyling(obj)
+    // }else{
+    //     let obj = {
+    //         color:'white',
+    //         backgroundColor:'black'
+    //     }
+    //     settextAreastyling(obj)
+    // }
+
     const makeUpper = () => {
         let n = text.toUpperCase();
         setUtext(n)
     }
 
-    const clear = () =>{
+    const clear = () => {
         setText("")
         setUtext("")
         setReverse("")
     }
 
-    const makeReverse = ()=>{
-        let newText = text.split("").reverse().join("")       
+    const makeReverse = () => {
+        let newText = text.split("").reverse().join("")
         setReverse(newText)
     }
 
-    const reversewords = () =>{
-        let newText = text.split(" ").reverse().join(" ")   
+    const reversewords = () => {
+        let newText = text.split(" ").reverse().join(" ")
         setReverse(newText)
     }
 
@@ -38,7 +55,7 @@ export default function TextForm(props) {
                     {props.heading}
                 </h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={onchangehandler} value={text} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" style={textAreastyling} onChange={onchangehandler} value={text} id="myBox" rows="8"></textarea>
                 </div>
 
                 <button className="btn btn-primary mx-1" onClick={makeUpper}> Convert to Uppercase </button>
@@ -50,7 +67,7 @@ export default function TextForm(props) {
             <div className="container my-3">
                 <h1> Text Summary</h1>
                 <p>
-                    {text.split(' ').length } words and {text.length} characters
+                    {text.split(' ').length} words and {text.length} characters
                 </p>
                 <p>
                     {reverseText}
