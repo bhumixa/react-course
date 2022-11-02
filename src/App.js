b5c00421443dd947b14cfd412bc2fc6a2569a962
bhumixa <bhumi.patel2512@gmail.com>
2022-11-02 19:08:07 +0530

    about us component with dynamic css changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
-    <Navbar title="h1"/>
-    <TextForm heading="Enter Text" ></TextForm>
+      <Navbar title="h1"/>
+      <About />
+      {/* <TextForm heading="Enter Text" ></TextForm> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min.js'; // Have to install and import jQuery because of bootstrap modal's dependency
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
